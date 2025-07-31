bb13f037c7ba263bec8dabc0a8b7b3a209db0a3c
Wysted <vladimirbesser@gmail.com>
2025-07-30 20:43:17 -0400

    [TEST]

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
-    base: '/portafolio/',
+    base: '/',
     vite: {
         plugins: [tailwindcss()],
     },
